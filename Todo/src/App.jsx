10b63a36afef82c613af43038006639279b5d778
hannasherin <hannasherinpp2000@gmail.com>
2026-07-26 21:47:26 +0530

    React Activity

---
+++ Todo/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+
+  return (
+    <div className="main">
+
+<h1>Todo List</h1>
+<hr />
+<input type="text"
+placeholder="Add New Task" />
+
+<button>Add</button>
+
+
+    </div>
+     
+  )
+}
+
+export default App
