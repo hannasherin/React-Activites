7e6b52790b491bb765950b4cb217e6ef81f73294
hannasherin <hannasherinpp2000@gmail.com>
2026-07-27 21:32:07 +0530

    React Activity

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react"
 
 
 function App() {
+  const[inputvalue,setInputvalue]=useState('')
+  const[data,setData]=useState([])
+  
+  const handlechange=(e)=>{
+    setInputvalue(e.target.value)
+  }
+
+  const addTask = () => {
+    if (inputValue.trim()) {
+      setData([...data, inputValue])
+      setInputvalue('')
+    }
+
+  }
+
+  const deleteTask = (index) => {
+
+    const is_confirm = window.confirm("Are  sure want to delete this?")
+    if (is_confirm) {
+      const datas = data.filter((_, inx) => inx != index)
+      setData(datas)
+      alert("delete Succesfully")
+    }
+  }
 
   return (
     <div className="main">
@@ -8,9 +33,21 @@ function App() {
 <h1>Todo List</h1>
 <hr />
 <input type="text"
-placeholder="Add New Task" />
+placeholder="Add New Task"
+value={inputvalue}
+onChange={handlechange}
+ />
 
-<button>Add</button>
+<button onClick={addTask}>Add Task</button> 
+
+<ul>
+  {data.map((item,index)=>(
+    <li key={index}>
+      {item}
+      <button onClick={()=>deleteTask(index)}>Delete</button>
+    </li>
+  ))}
+</ul>
 
 
     </div>
